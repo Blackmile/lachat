6a73a0d7abda0ca73ec28002eb01730eeae043cf
ucheBest <stanley.ezeifeanyi@gmail.com>
2022-08-20 16:31:15 +0100

    created components and screens

---
+++ src/components/chatroom/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Chatroom = () => {
+  return (
+    <View>
+      <Text>click here for to chat with friends</Text>
+    </View>
+  )
+}
+
+export default Chatroom
