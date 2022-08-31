6b7d04454cf30336fb8b67223d727fd93c318b98
ucheBest <stanley.ezeifeanyi@gmail.com>
2022-08-31 23:14:36 +0100

    added sign out btn

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { signOut } from 'firebase/auth'
+import { auth } from '../utils/firebase'
+
 
 const HomeScreen = () => {
+  const SignOut = async () => {
+      try {
+        await signOut(auth)
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
   return (
     <View>
       <Text>HomeScreen</Text>
+      <View style={{marginTop: 20}}>
+            <TouchableOpacity>
+              <Button
+                title='Sign out' 
+                color='black'
+                onPress={SignOut}
+              />
+            </TouchableOpacity>
+      </View>
     </View>
   )
 }
