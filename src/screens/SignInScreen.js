31cbdeba08b71fe762c4c113d565c301e7690356
ucheBest <stanley.ezeifeanyi@gmail.com>
2022-09-01 00:14:28 +0100

    added login and logic to switch between login and sign up mode

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -2,18 +2,46 @@ import { View, Text, StyleSheet, Button, TouchableOpacity, ImageBackground } fro
 import React, { useState } from 'react'
 import { TextInput } from 'react-native-gesture-handler'
 import backgroundImg from '../../assets/backgroundImg.png';
-import { signUp } from '../utils/firebase';
+import { signIn, signUp } from '../utils/firebase';
 
 const SignInScreen = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [checkPass, setCheckPass] = useState('')
+  const [mode, setMode] = useState('Sign In')
+  let displayCheckPass = null;
+
+  const handleMode = () => {
+    return (
+      setMode(mode === 'Sign In' ? 'Sign Up' : "Sign In")
+    )
+  }
+
+  if (mode === 'Sign Up') {
+    displayCheckPass = (
+      <View>
+        <TextInput
+              placeholder='confirm password'
+              value={checkPass}
+              onChangeText={setCheckPass}
+              secureTextEntry={true} 
+              style={styles.textInput}
+          />
+      </View>
+    );   
+  }
 
   async function Submit() {
-    if (password !== checkPass) {
-      alert("invalid password")
+    if(mode === 'Sign Up'){
+      if (password !== checkPass) {
+        alert("😢, invalid password")
+      }else{
+        await signUp(
+          email, password
+        )
+      }
     }else{
-      await signUp(
+      await signIn(
         email, password
       )
     }
@@ -23,39 +51,43 @@ const SignInScreen = () => {
     <ImageBackground style={styles.backgroundImg} source={backgroundImg} blurRadius={3}>
       <View style={styles.container}>
         <Text style={styles.welcomeText}>
-          Sign In
+          {mode === 'Sign In' ? 'Login into your account' : 'Create a new account'}
         </Text>
         <View style={{marginTop: 20}}>
         <TextInput
-            placeholder='email'
+            placeholder='Enter Email'
             value={email}
             onChangeText={setEmail} 
             style={styles.textInput}
         />
         <TextInput
-            placeholder='password'
+            placeholder='Enter Password'
             value={password}
             onChangeText={setPassword}
             secureTextEntry={true} 
             style={styles.textInput}
         />
-        <TextInput
-            placeholder='password'
-            value={checkPass}
-            onChangeText={setCheckPass}
-            secureTextEntry={true} 
-            style={styles.textInput}
-        />
+        {displayCheckPass}
         <View style={{marginTop: 20}}>
           <TouchableOpacity>
             <Button 
-              title='Sign Up' 
+              title='Submit' 
               color='black'
               onPress={Submit}
               disabled={!email || !password}
               />
             </TouchableOpacity>
           </View>
+          <TouchableOpacity
+            style={{ marginTop: 15 }}
+            onPress={handleMode}
+          >
+            <Text>
+              {mode === "Sign In"
+                ? "Don't have an account? Sign Up"
+                : "Already have an account? Sign in"}
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
     </ImageBackground>
@@ -81,7 +113,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderStyle: 'solid',
     width: 250,
-    fontSize: 22,
+    fontSize: 20,
+    marginBottom: 20,
   },
   backgroundImg: {
     minWidth: '50%',
