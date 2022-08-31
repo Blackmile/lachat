99e840364a4bf43e05baeb1d5cd96817aab0dd25
ucheBest <stanley.ezeifeanyi@gmail.com>
2022-08-31 23:14:20 +0100

    signUp ui

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -2,37 +2,57 @@ import { View, Text, StyleSheet, Button, TouchableOpacity, ImageBackground } fro
 import React, { useState } from 'react'
 import { TextInput } from 'react-native-gesture-handler'
 import backgroundImg from '../../assets/backgroundImg.png';
+import { signUp } from '../utils/firebase';
 
 const SignInScreen = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [checkPass, setCheckPass] = useState('')
+
+  async function Submit() {
+    if (password !== checkPass) {
+      alert("invalid password")
+    }else{
+      await signUp(
+        email, password
+      )
+    }
+  }
 
   return (
-    <ImageBackground style={styles.backgroundImg} source={backgroundImg}>
+    <ImageBackground style={styles.backgroundImg} source={backgroundImg} blurRadius={3}>
       <View style={styles.container}>
         <Text style={styles.welcomeText}>
           Sign In
         </Text>
         <View style={{marginTop: 20}}>
-          <TextInput 
-              placeholder='email'
-              value={email}
-              onChangeText={setEmail} 
-              style={styles.textInput}
-              textContentType='emailAddress'
-          />
-          <TextInput
-              placeholder='password'
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry={true} 
-              style={styles.textInput}
-          />
-          <View style={{marginTop: 20}}>
-            <TouchableOpacity>
-              <Button
-                title='Sign Up' 
-                color='black'
+        <TextInput
+            placeholder='email'
+            value={email}
+            onChangeText={setEmail} 
+            style={styles.textInput}
+        />
+        <TextInput
+            placeholder='password'
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={true} 
+            style={styles.textInput}
+        />
+        <TextInput
+            placeholder='password'
+            value={checkPass}
+            onChangeText={setCheckPass}
+            secureTextEntry={true} 
+            style={styles.textInput}
+        />
+        <View style={{marginTop: 20}}>
+          <TouchableOpacity>
+            <Button 
+              title='Sign Up' 
+              color='black'
+              onPress={Submit}
+              disabled={!email || !password}
               />
             </TouchableOpacity>
           </View>
@@ -60,12 +80,14 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
     borderBottomWidth: 1,
     borderStyle: 'solid',
-    width: 300
+    width: 250,
+    fontSize: 22,
   },
   backgroundImg: {
     minWidth: '50%',
     flex: 1,
   }
 })
+
 
 export default SignInScreen
