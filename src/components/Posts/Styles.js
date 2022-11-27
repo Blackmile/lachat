9c5893d5acffd724fa4545edfac7ee1d77506ff1
ucheBest <stanley.ezeifeanyi@gmail.com>
2022-11-27 02:06:40 +0100

    created separate files for styles

---
+++ src/components/Posts/Styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const Styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        paddingLeft: 30,
+        paddingTop: 10,
+    },
+    posts: {
+        alignItems: 'center',
+        padding: 50,
+        backgroundColor: '#f6f6f6',
+        justifyContent: 'center',
+        alignContent: 'center',
+    }
+})
+
+export default Styles
